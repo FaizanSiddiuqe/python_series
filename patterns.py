6ac904caa62fccf6e37f5a1225c4c148bf149fc2
FaizanSiddiuqe <154720299+FaizanSiddiuqe@users.noreply.github.com>
2024-11-01 20:20:14 +0500

    patterns using for loop

---
+++ patterns.py
@@ -0,0 +1,25 @@
+### Print React angle
+
+##using for loop
+
+for i in range(4):
+    for j in range(4):
+        print("*", end=" ")
+    print()
+
+
+### another method
+n = 4
+m  = 4
+for i in range(n):
+    print("* " * m)
+
+
+## print triangle
+for i in range(4):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+    
+
+    
