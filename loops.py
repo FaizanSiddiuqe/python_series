de8261fd26108d6984b13dc2d85db51b6f44d5f0
FaizanSiddiuqe <154720299+FaizanSiddiuqe@users.noreply.github.com>
2024-11-01 18:02:49 +0500

    while loop and for loop

---
+++ loops.py
@@ -0,0 +1,26 @@
+## In the python we have only two loops
+#1. for loop
+#2. While loop
+
+### In the for loop we use range keyword which is built function of the python which defined the width of the for loop
+### when we use range here in it same concept like another language like c++ etc
+### loop will start from zero if we don't initialize it  and would go to range-1
+### SYNTAX
+
+for i in range(5):
+    print(i)
+
+## print the my name three times
+
+for name in range(3):
+    print("Faizan Siddique")
+
+## while loop 
+## its similar to another language while loop
+#here is the example
+###SYNTAX
+
+i = 1
+while(i <= 3):
+    print(i)
+    i +=1
