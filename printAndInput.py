0f0625c21e44a7a9acce8fdc5db513868ebded50
FaizanSiddiuqe <154720299+FaizanSiddiuqe@users.noreply.github.com>
2024-11-01 17:07:44 +0500

    how to print and how to take input from user

---
+++ printAndInput.py
@@ -0,0 +1,24 @@
+
+## how to take input and print 
+name = "Faizan Siddique"
+print("My name is ",name)   ## use print statement && use comma to concate
+
+## how to take input from the users
+
+name = input("Enter your name:: ")
+print("You are ",name)
+
+ ## NOte => in the python when we take any input its by default in the string form
+ 
+num = int(input("Enter number"))   
+## we are converting into interger data type using  int
+print("You entered num", num)
+
+## another method to take input in integer
+num1 = input("Enter your number")
+
+## here we will convert into integer data type
+num1 = int(num1)
+
+## now print the number
+print("You entered ",num1)
